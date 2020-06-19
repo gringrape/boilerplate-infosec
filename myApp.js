55f1c5efe51b1773137291ed8112233bceeffb23
gringrape <gringrape200@gmail.com>
2020-06-20 06:39:16 +0900

    task11

--- myApp.js
+++ myApp.js
@@ -181,7 +181,8 @@ app.use(helmet.noCache());
 
 app.use(helmet.contentSecurityPolicy({
   directives: {
-    defaultSrc: ["'self'", 'trusted-cdn.com']
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com']
   }
 }))
 
